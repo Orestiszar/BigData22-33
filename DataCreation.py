64e3c410dda35f31bd1f1071c8acda196b7453b0
tsiakag <72927884+tsiakag@users.noreply.github.com>
2023-02-08 13:35:55 +0200

    integration of kafka in DataCreation.py
    -> intergrated kafka producer module
    -> changes in some methods to match kafka messaging requirements
    -> code cleanup

--- DataCreation.py
+++ DataCreation.py
@@ -1,54 +1,116 @@
 import random
 import datetime
+from sqlite3 import Timestamp
 import time
+from matplotlib.font_manager import json_dump
 import numpy as np
+import json
+
+# kafka imports and config
+from confluent_kafka import Producer
+import socket
+
+from pydantic import KafkaDsn
+from kafka_test.kafka_config import conf
+conf['client.id'] = socket.gethostname() + "_data_producer"
+
+# used for acknowledgment if the msg was produced succesfully 
+def kafka_callback(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg.value()), str(err)))
+    else:
+        print("Message produced: %s" % (str(msg.value())))
+
+def kafka_produce(data_dict, timestamp):
+    global key
+    for name, value in data_dict.items():
+        json_temp = {
+            "name" : name, 
+            "value" : value,
+            "timestamp" : str(timestamp)
+        }
+        # cast to json
+        msg = json.dumps(json_temp)
+    
+        producer.produce('test_topic', key=str(key), value=msg, callback=kafka_callback)
+        # TODO: check with callback if the message was really delivered
+        producer.poll(2)
+        key += 1
+
 
 def create_data_15min():
-    TH1=np.random.uniform(12,35)
-    TH2=np.random.uniform(12,35)
-    HVAC1=np.random.uniform(0,100)
-    HVAC2=np.random.uniform(0,200)
-    MiAC1=np.random.uniform(0,150)
-    MiAC2=np.random.uniform(0,200)
-    W1=np.random.uniform(0,1)
-    return (TH1,TH2,HVAC1,HVAC2,MiAC1,MiAC2,W1)
+    data_dict = {}
     
+    data_dict['TH1']=np.random.uniform(12,35)
+    data_dict['TH2']=np.random.uniform(12,35)
+    
+    data_dict['HVAC1']=np.random.uniform(0,100)
+    data_dict['HVAC2']=np.random.uniform(0,200)
+
+    data_dict['MiAC1']=np.random.uniform(0,150)
+    data_dict['MiAC2']=np.random.uniform(0,200)
+    
+    data_dict['W1']=np.random.uniform(0,1)
+    
+    return data_dict
+    
+
 def create_data_1day():
     global wtotenergy, etotenergy
+    
     etot = np.random.uniform(-1000,1000)
     etotenergy += (2600*24 + etot)
+    
     wtot = np.random.uniform(-10,10)
     wtotenergy += (100+wtot)
 
+
+#TODO: remove(?)
 def MOV1_old():
     return np.random.uniform(0,1)
 
+# used for picking random samples during the day(?)
 moving_list = [1 if i < 5 else 0 for i in range(96)]
 random.shuffle(moving_list)
 
+# init metric values and date
 date=datetime.datetime(2023,1,1)
 etotenergy=0
 wtotenergy=0
 secondcounter=0
 
+# create the producer used for kafka
+producer = Producer(conf)
+
+# key counter for kafka (used just in case)
+key = 0
+
 while(1):
+    # produce if it is time for MOV1
     if(moving_list[secondcounter%96]==1):
         tempdate = date + datetime.timedelta(minutes=random.randint(0,15),seconds=random.randint(0,60))
-        print(tempdate,"1")
-    if(secondcounter%96 == 0 and secondcounter!=0):
+        
+        kafka_produce({'MOV1' : 1}, tempdate)
+
+    # produce required data at the end of each day
+    if(secondcounter%96 == 0 and secondcounter != 0):
         create_data_1day()
-        print(etotenergy, wtotenergy, "Called")
+        
         random.shuffle(moving_list)
-    if(secondcounter%20==0 and secondcounter!=0):
-        tempdate_mov = date-datetime.timedelta(days=2)
-        print(MOV1_old(),tempdate_mov)
-    if(secondcounter%120==0 and secondcounter!=0):
-        tempdate_mov = date-datetime.timedelta(days=10)
-        print(MOV1_old(),tempdate_mov)
-    # (TH1,TH2,HVAC1,HVAC2,MiAC1,MiAC2,W1) = create_data_15min()
-    print(create_data_15min())
-    date+=datetime.timedelta(minutes=15)
-    # print(date)
-    #feed ston kafka
+        kafka_produce({'Etot' : etotenergy, 'Wtot': wtotenergy}, date)
+        
+        
+    # if(secondcounter%20==0 and secondcounter!=0):
+    #     tempdate_mov = date-datetime.timedelta(days=2)
+    #     print(MOV1_old(),tempdate_mov)
+
+    # if(secondcounter%120==0 and secondcounter!=0):
+    #     tempdate_mov = date-datetime.timedelta(days=10)
+    #     print(MOV1_old(),tempdate_mov)
+
+    # produce data for each 15 min
+    kafka_produce(create_data_15min(), date)
+    date += datetime.timedelta(minutes=15)
+    
     secondcounter+=1
     time.sleep(1)
