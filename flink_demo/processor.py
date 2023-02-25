5fdfbb6fe754d7e6a26093cd1b4349283e937345
tsiakag <72927884+tsiakag@users.noreply.github.com>
2023-02-25 20:35:49 +0200

    java no longer my best friend, python returned

--- flink_demo/processor.py
+++ flink_demo/processor.py
@@ -2,74 +2,43 @@ import os
 
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.table import StreamTableEnvironment, EnvironmentSettings
-from pyflink.common import Row
-from pyflink.common.serialization import JsonRowDeserializationSchema, JsonRowSerializationSchema
-from pyflink.common.typeinfo import Types
-from pyflink.datastream import StreamExecutionEnvironment
-from pyflink.datastream.connectors import FlinkKafkaConsumer, FlinkKafkaProducer
-
 
 
 def main():
     # Create streaming environment
     env = StreamExecutionEnvironment.get_execution_environment()
 
-    env.add_jars("file:///home/orestis/Desktop/BigData22-33/flink_demo/flink-sql-connector-kafka_2.12-1.13.6.jar")
-
     settings = EnvironmentSettings.new_instance()\
                       .in_streaming_mode()\
+                      .use_blink_planner()\
                       .build()
 
     # create table environment
     tbl_env = StreamTableEnvironment.create(stream_execution_environment=env,
                                             environment_settings=settings)
-    
-    deserialization_schema = JsonRowDeserializationSchema.builder() \
-        .type_info(type_info=Types.ROW([Types.LONG(), Types.LONG()])).build()
 
-    kafka_source = FlinkKafkaConsumer(
-        topics='test_topic',
-        deserialization_schema=deserialization_schema,
-        properties={
-            'bootstrap.servers':"pkc-w7d6j.germanywestcentral.azure.confluent.cloud:9092",
-            'security.protocol':"SASL_SSL",
-            'sasl.mechanisms':"PLAIN",
-            'sasl.username':"ITVCKFDO6EJFOGHD",
-            'sasl.password':"BFmFxMtT1w9ciCxg99DnfI+YWR/Jv+nc4w3X50+4gkOToJJ7yUcRhR1HlkTyVMgM",
-            'client.id':'flink-consumer',
-            'group.id':'flink-group'
-        })
-
-    ds = env.add_source(kafka_source)
     # add kafka connector dependency
-    # kafka_jar = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-    #                         'flink-sql-connector-kafka_2.12-1.13.6.jar')
+    kafka_jar = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                            'flink-sql-connector-kafka_2.12-1.13.6.jar')
 
-
-    # tbl_env.get_config()\
-    #         .get_configuration()\
-    #         .set_string("pipeline.jars", "file://{}".format(kafka_jar))
-
+    tbl_env.get_config()\
+            .get_configuration()\
+            .set_string("pipeline.jars", "file://{}".format(kafka_jar))
 
     #######################################################################
     # Create Kafka Source Table with DDL
     #######################################################################
     src_ddl = """
-        CREATE TABLE sensor_input (
-            m_name VARCHAR,
-            m_value DOUBLE,
-            m_timestamp VARCHAR
+        CREATE TABLE sales_usd (
+            seller_id VARCHAR,
+            amount_usd DOUBLE,
+            sale_ts BIGINT,
+            proctime AS PROCTIME()
         ) WITH (
             'connector' = 'kafka',
-            'topic' = 'test_topic',
-            'properties.group.id' = 'testgroup',
-            'properties.client.id'='testclient',
-            'properties.bootstrap.servers'='pkc-w7d6j.germanywestcentral.azure.confluent.cloud:9092',
-            'properties.security.protocol'='SASL_SSL',
-            'properties.sasl.mechanisms'='PLAIN',
-            'properties.sasl.username'='ITVCKFDO6EJFOGHD',
-            'properties.sasl.password'='BFmFxMtT1w9ciCxg99DnfI+YWR/Jv+nc4w3X50+4gkOToJJ7yUcRhR1HlkTyVMgM',
-            'properties.sasl.jaas.config' = 'org.apache.kafka.common.security.plain.PlainLoginModule required username="ITVCKFDO6EJFOGHD" password="BFmFxMtT1w9ciCxg99DnfI+YWR/Jv+nc4w3X50+4gkOToJJ7yUcRhR1HlkTyVMgM" serviceName="kafka";',
+            'topic' = 'sales-usd',
+            'properties.bootstrap.servers' = 'localhost:9092',
+            'properties.group.id' = 'sales-usd',
             'format' = 'json'
         )
     """
@@ -77,7 +46,7 @@ def main():
     tbl_env.execute_sql(src_ddl)
 
     # create and initiate loading of source Table
-    tbl = tbl_env.from_path('sensor_input')
+    tbl = tbl_env.from_path('sales_usd')
 
     print('\nSource Schema')
     tbl.print_schema()
@@ -87,10 +56,13 @@ def main():
     #####################################################################
     sql = """
         SELECT
-          m_name,
-          m_value,
-          m_timestamp
-        FROM sensor_input
+          seller_id,
+          TUMBLE_END(proctime, INTERVAL '60' SECONDS) AS window_end,
+          SUM(amount_usd) * 0.85 AS window_sales
+        FROM sales_usd
+        GROUP BY
+          TUMBLE(proctime, INTERVAL '60' SECONDS),
+          seller_id
     """
     revenue_tbl = tbl_env.sql_query(sql)
 
@@ -98,23 +70,27 @@ def main():
     revenue_tbl.print_schema()
 
     ###############################################################
-    # Create Sink Table
+    # Create Kafka Sink Table
     ###############################################################
     sink_ddl = """
-        CREATE TABLE output_print (
-            m_name VARCHAR,
-            m_val DOUBLE,
-            m_timestamP VARCHAR
+        CREATE TABLE sales_euros (
+            seller_id VARCHAR,
+            window_end TIMESTAMP(3),
+            window_sales DOUBLE
         ) WITH (
-            'connector'='print'
+            'connector' = 'kafka',
+            'topic' = 'sales-euros',
+            'properties.bootstrap.servers' = 'localhost:9092',
+            'format' = 'json'
         )
     """
     tbl_env.execute_sql(sink_ddl)
 
     # write time windowed aggregations to sink table
-    revenue_tbl.execute_insert('output_print').wait()
+    revenue_tbl.execute_insert('sales_euros').wait()
 
-    tbl_env.execute('windowed-output-print')
+    tbl_env.execute('windowed-sales-euros')
+
 
 if __name__ == '__main__':
     main()
--- my-flink-project/src/main/java/myflink/StreamingJob.java
+++ my-flink-project/src/main/java/myflink/StreamingJob.java
@@ -2,7 +2,11 @@ package myflink;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.connector.base.DeliveryGuarantee;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
@@ -17,6 +21,17 @@ import org.apache.hadoop.hbase.HColumnDescriptor;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.
+
+
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.api.java.*;
+import org.apache.flink.streaming.api.environment.*;
+import org.apache.flink.table.api.*;
+import org.apache.flink.table.api.bridge.java.*;
+import org.apache.flink.table.catalog.hbase.*;
+import org.apache.flink.table.descriptors.*;
+import org.apache.flink.types.Row;
 
 import java.io.IOException;
 
@@ -28,6 +43,7 @@ public class StreamingJob {
 
         StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 
+        //region Kafka Source datastream
         KafkaSource<Myrow> source = KafkaSource.<Myrow>builder()
                 .setBootstrapServers("localhost:9092")
                 .setTopics("quickstart-new")
@@ -51,103 +67,39 @@ public class StreamingJob {
                 })
                 .build();
         DataStream<Myrow> dataStream = env.fromSource(source,WatermarkStrategy.noWatermarks(),"Kafka Source");
-
+        //endregion
 
         tableEnv.createTemporaryView("MyRows", dataStream);
-//        tableEnv.executeSql("SELECT m_name FROM MyRows");
 
-        TableResult result = tableEnv.executeSql("SELECT m_timestamp FROM MyRows");
+        Table table = tableEnv.sqlQuery("SELECT m_timestamp FROM MyRows");
+        TableResult result = table.execute();
 
         for (CloseableIterator<Row> it = result.collect(); it.hasNext(); ) {
             Row row = it.next();
             System.out.println(row.getField("m_timestamp"));
         }
 
-//        String temp_hbase = "CREATE TABLE hTable (\n" +
-//                " rowkey INT,\n" +
-//                " cf ROW<q1 STRING, q2 STRING, q3 STRING>,\n" +
-//                " PRIMARY KEY (rowkey) NOT ENFORCED\n" +
-//                ") WITH (\n" +
-//                " 'connector' = 'hbase-2.2',\n" +
-//                " 'table-name' = 'test',\n" +
-//                " 'zookeeper.quorum' = 'localhost:2181'\n" +
-//                ");";
-//
-//        tableEnv.executeSql(temp_hbase);
-//        String temp_insert = "INSERT INTO hTable\n" +
-//                "(rowkey, cf) VALUES ('1', 'test');\n";
-//        tableEnv.executeSql(temp_insert);
-
-
-//        TODO: check if needed
-        // Instantiating configuration class
-        Configuration con = HBaseConfiguration.create();
-
-        // Instantiating HbaseAdmin class
-        HBaseAdmin admin = new HBaseAdmin(con);
-
-        // Instantiating table descriptor class
-        HTableDescriptor tableDescriptor = new
-                HTableDescriptor(TableName.valueOf("emp"));
-
-        // Adding column families to table descriptor
-        tableDescriptor.addFamily(new HColumnDescriptor("personal"));
-        tableDescriptor.addFamily(new HColumnDescriptor("professional"));
-
-        // Execute the table through admin
-        admin.createTable(tableDescriptor);
-        System.out.println(" Table created ");
-
-
+        DataStream<Row> kafkaSinkStream = tableEnv.toDataStream(table);
 
         env.execute("Simple SQL query");
 
+        //region Kafka sink
+        KafkaSink<String> sink = KafkaSink.<String>builder()
+                .setBootstrapServers("localhost:9092")
+                .setRecordSerializer(KafkaRecordSerializationSchema.builder()
+                        .setTopic("hbase-flink")
+                        .setValueSerializationSchema(new SimpleStringSchema())
+                        .build()
+                )
+                .setDeliveryGuarantee(DeliveryGuarantee.AT_LEAST_ONCE)
+                .build();
 
+        dataStream.sinkTo(sink);
+        //endregion
     }
-   /* public static void main(String[] args) {
-        // set up the streaming execution environment
-        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        // create a sample stream of data as Tuple2<String, Integer>
-        DataStream<Tuple2<String, Integer>> stream = env.fromElements(
-                new Tuple2<>("key1", 10),
-                new Tuple2<>("key2", 20),
-                new Tuple2<>("key3", 30));
 
-        // set up the HBaseTableSchema for the HBase table
-        HBaseTableSchema schema = new HBaseTableSchema();
-        schema.addColumn("cf1", "key", Bytes.toBytes("UTF-8"));
-        schema.addColumn("cf1", "value", Bytes.toBytes("UTF-8"));
 
-        // set up the HBaseTableDescriptor for the HBase table
-        HTableDescriptor tableDescriptor = new HTableDescriptor(Table"example_table", schema);
-
-        // set up the HBaseSinkFunction to write data to the HBase table
-        HBaseSinkFunction<Tuple2<String, Integer>> hbaseSinkFunction = new HBaseSinkFunction<>(
-                tableDescriptor,
-                new SerializableHBaseFunction<Tuple2<String, Integer>>() {
-                    @Override
-                    public void apply(Tuple2<String, Integer> value, Put put) {
-                        // set the row key
-                        put.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("key"), Bytes.toBytes(value.f0));
-
-                        // set the value
-                        put.addColumn(Bytes.toBytes("cf1"), Bytes.toBytes("value"), Bytes.toBytes(value.f1));
-                    }
-                },
-                new RowDataConverter());
-
-        // write the stream data to the HBase table using HBaseSink
-        stream.map(new MapFunction<Tuple2<String, Integer>, Tuple2<String, Integer>>() {
-            @Override
-            public Tuple2<String, Integer> map(Tuple2<String, Integer> value) throws Exception {
-                return value;
-            }
-        }).addSink(new HBaseSink<>(hbaseSinkFunction));
-
-        // execute the Flink program
-        env.execute("Flink HBase Example");
-    }*/
     // LEGACY
 //    public static class Splitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
 //        @Override
